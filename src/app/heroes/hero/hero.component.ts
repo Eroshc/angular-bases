496c8b9156d6079883e3d63d6cfb406b3bf8446b
Eros Silva <eros.silva_hc@outlook.com>
2024-11-08 23:37:49 -0300

    Fin de seccion 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name: string='ironman';
+  public age: number = 44;
+
+  get capitalizedName ():string{
+    return this.name.toUpperCase();
+  }
+  public getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+  changeHero():void{
+    this.name ='Spiderman';
+  }
+  changeAge():void{
+    this.age=23;
+  }
+  Reset():void{
+    this.name="Ironman";
+    this.age=44;
+
+  }
+
+}
